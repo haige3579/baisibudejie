205edcdba7eda1fffc77cd4aae04eab8db4934c2
CYH <58933405@qq,com>
2016-09-07 21:47:01 +0800

    1  百思不得姐

---
+++ app/src/main/java/com/example/cyh/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cyh.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 1  as下载/资源网站：www.androiddevtool.cn
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
